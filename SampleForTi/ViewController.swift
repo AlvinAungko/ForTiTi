e98ef4af7232a4e8a64af3b2a4874ef5d31d0551
Aung Ko <alieaungko@gmail.com>
2024-01-18 20:58:22 +0630

    Chore: Set up module

---
+++ SampleForTi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleForTi
+//
+//  Created by Aung Ko Ko on 18/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
